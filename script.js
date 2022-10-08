e821280b35746dbfa651270640b34e7ba11ed12c
Nate Cramer <natecramer@gmail.com>
2022-10-08 14:26:22 -0400

    Add basic interface to start game

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 console.log('Welcome to my game');
 
+/**
+ * Generates a random choice of 'rock' 'paper' or 'scissors'
+ * @returns String 
+ */
 function getComputerChoice() {
     let r = Math.floor(Math.random() * 2);
     if (r === 0) {
@@ -11,6 +15,12 @@ function getComputerChoice() {
     }
 }
 
+/**
+ * Pass in Player and Computer choices for 'rock' 'paper' or 'scissors', and this will evaluate and tell you who wins.
+ * @param {string} playerChoice 'rock' 'paper' 'scissors'
+ * @param {string} computerChoice 'rock' 'paper' 'scissors'
+ * @returns String for who wins, either 'Player' or 'Computer'
+ */
 function evaluate(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
         return 'Draw';
@@ -42,11 +52,14 @@ function evaluate(playerChoice, computerChoice) {
     return 'Computer';
 }
 
+/**
+ * 
+ * @returns string for who won the round, either 'Player' or 'Computer'
+ */
 function playRound() {
     let playerChoice = String(prompt('rock, paper or scissors?'));
     playerChoice = playerChoice.toLocaleLowerCase();
-    // playerChoice = playerChoice.slice(0,1).toUpperCase() + playerChoice.slice(1,playerChoice.length).toLowerCase();
-
+    
     computerChoice = getComputerChoice();
     console.log(`The player chose ${playerChoice}`);
     console.log(`The computer chose ${computerChoice}`);
@@ -56,10 +69,12 @@ function playRound() {
     return winner;
 }
 
+// main game function, this is called once.
 function game() {
     let playerScore = 0;
     let computerScore = 0;
 
+    // Main game loop
     for (i = 1; i <= 5; i++) {
         console.log(`Round ${i}/5`)
         let result = playRound();
@@ -71,6 +86,7 @@ function game() {
         console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}\n`);
     }
 
+    // Game end
     console.log('Game over!');
     console.log(`The final score is ${playerScore} to ${computerScore}`);
     if (playerScore > computerScore) {
@@ -82,4 +98,4 @@ function game() {
     }
 }
 
-game();
+document.querySelector('#playButton').addEventListener('click', game);
